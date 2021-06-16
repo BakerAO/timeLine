fb276fa57a867fc0537096cde9e30f7210cfdb3e
BakerAO <dpshunter@gmail.com>
2021-06-16 09:19:53 -0500

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import Header from './Header'
+import Content from './content/Content'
 
 export default function App() {
   return (
     <div>
       <Header />
-      App
+      <Content />
     </div>
   )
 }
