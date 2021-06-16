4a49408dfce28dbfa6e58e272fd7feef1247a625
BakerAO <dpshunter@gmail.com>
2021-06-16 09:14:58 -0500

    Update

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,21 +15,55 @@ const styles = {
   },
   right: {
     display: 'flex',
+    justifyContent: 'flex-end',
     flex: 1,
+  },
+  rightSpacer: {
+    display: 'flex',
+    flex: 4
+  },
+  rightItem: {
+    display: 'flex',
+    justifyContent: 'center',
+    flex: 1,
+    margin: '10px 0 0 0'
+  },
+  p: {
+    margin: '0 0 0 0',
+    border: '1px solid white',
+    height: '20px',
+    padding: '5px'
   }
 }
 
 export default function Header() {
-  return (
-    <div style={styles.container}>
+  const renderLeft = () => {
+    return (
       <div style={styles.left}>
         <h1>TimeLine</h1>
         <div>Nav/Options</div>
       </div>
+    )
+  }
+
+  const renderRight = () => {
+    return (
       <div style={styles.right}>
-        <div>TimeLine</div>
-        <div>Nav/Options</div>
+        <div style={styles.rightSpacer} />
+        <div style={styles.rightItem}>
+          <p style={styles.p}>Account</p>
+        </div>
+        <div style={styles.rightItem}>
+          <p style={styles.p}>Settings</p>
+        </div>
       </div>
+    )
+  }
+
+  return (
+    <div style={styles.container}>
+      {renderLeft()}
+      {renderRight()}
     </div>
   )
 }
